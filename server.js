c55a4eccf3aa0c533a7461ad4165baefe403cbc6
Praveenkumar284 <praveenkumaroy121@gmail.com>
2024-08-02 01:13:21 +0530

    change

--- server.js
+++ server.js
@@ -11,22 +11,19 @@ app.use(cors());
 
 app.get('/api/vpn-status', async (req, res) => {
   const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const API_URL = `https://ipqualityscore.com/api/json/ip/${IPQUALITYSCORE_API_KEY}/${userIp}`;
-
-  console.log(`Fetching data from: ${API_URL}`);
 
   try {
-    const response = await axios.get(API_URL);
-    console.log('API Response:', response.data);  // Log the API response
+    const response = await axios.get(`https://ipqualityscore.com/api/json/ip/${IPQUALITYSCORE_API_KEY}/${userIp}`);
     const data = response.data;
+
     const isUsingVPN = data.proxy;
+
     res.json({ isUsingVPN });
   } catch (error) {
-    console.error('Error fetching VPN status:', error.response ? error.response.data : error.message);
-    res.status(500).json({ isUsingVPN: false });
+    console.error('Error fetching VPN status:', error);
+    res.status(500).json({ error: error });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
